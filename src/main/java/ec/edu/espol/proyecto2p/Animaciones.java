7287a59abc7fb8aa18147bc22464f3c0dce57b39
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-01-09 23:53:45 -0500

    Animacion trucha xd

--- src/main/java/ec/edu/espol/proyecto2p/Animaciones.java
+++ src/main/java/ec/edu/espol/proyecto2p/Animaciones.java
@@ -1,49 +1,64 @@
 package ec.edu.espol.proyecto2p;
 
-import java.time.Duration;
-import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.scene.control.Button;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.layout.HBox;
-import javafx.animation.TranslateTransition;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
-public abstract class Animaciones extends Animation{
-    public static void animar_boton(Button button){
+public abstract class Animaciones {
+    public static void animar_boton(Button button) {
         button.setOnMouseEntered((event) -> {
             button.setBlendMode(BlendMode.HARD_LIGHT);
         });
-        
+
         button.setOnMouseExited((event) -> {
             button.setBlendMode(null);
         });
     }
-    
-    public static void desanimar_boton(Button button){
+
+    public static void desanimar_boton(Button button) {
         button.setOnMouseEntered((event) -> {
             button.setBlendMode(null);
         });
     }
-    
-    public static void mover_ficha_button(FichaButton fichaButton, HBox HboxContainer, int position){
-        // Crear una transición de posición
-        javafx.util.Duration drtn = new javafx.util.Duration(2);
-        TranslateTransition transition = new TranslateTransition(drtn, fichaButton);
-        // Establecer la posición final (puedes ajustar estos valores según tus necesidades)
-        transition.setToX(0);
-        transition.setToY(0);
 
-        // Configurar cualquier otra opción de la transición si es necesario
-        transition.setCycleCount(1);
-
-        // Manejar el evento de finalización de la transición si es necesario
-        transition.setOnFinished(event -> {
-            // Lógica que deseas ejecutar después de mover la ficha al HBox
-            HboxContainer.getChildren().add(position, fichaButton);
-            HboxContainer.requestLayout();
-            fichaButton.setStyle("-fx-background-color: black");
-        });
-
-        // Iniciar la transición
-       transition.play();
+    public static void desactivar_boton(Button button) {
+        button.setBlendMode(BlendMode.DARKEN);
     }
+
+    // Método usado en Tablero
+    public static void mover_ficha_button(FichaButton fichaButton, VBox origen, HBox destino, int position) {
+     // Agregar la FichaButton al HBox en la posición deseada
+     destino.getChildren().add(position, fichaButton);
+
+     // Obtener las coordenadas X e Y del HBox destino en relación con su contenedor padre (TableroPanel)
+     // Eliminar la FichaButton del HBox
+     destino.getChildren().remove(fichaButton);
+
+     // Crear una transición de posición desde las coordenadas obtenidas
+     Duration duration = new Duration(1000);
+     TranslateTransition transition = new TranslateTransition(duration, fichaButton);
+
+     // Establecer la posición final en el destino
+     transition.setInterpolator(Interpolator.EASE_BOTH);
+     fichaButton.setStyle("-fx-background-color: black");
+
+     // Configurar cualquier otra opción de la transición si es necesario
+     transition.setCycleCount(1);
+
+     // Manejar el evento de finalización de la transición si es necesario
+     transition.setOnFinished(event -> {
+         // Lógica que deseas ejecutar después de mover la ficha al destino
+         fichaButton.setVisible(true);
+         destino.getChildren().add(position, fichaButton);
+     });
+
+     // Iniciar la transición
+     transition.play();
+ }
+
+
 }
--- src/main/java/ec/edu/espol/proyecto2p/Tablero.java
+++ src/main/java/ec/edu/espol/proyecto2p/Tablero.java
@@ -1,5 +1,5 @@
 package ec.edu.espol.proyecto2p;
-
+//
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
