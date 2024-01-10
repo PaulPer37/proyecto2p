eac168daa759a6c06766956f1820e861ca561dd2
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2024-01-09 21:26:52 -0500

    a

--- src/main/java/ec/edu/espol/proyecto2p/FichaButton.java
+++ src/main/java/ec/edu/espol/proyecto2p/FichaButton.java
@@ -1,6 +1,7 @@
 package ec.edu.espol.proyecto2p;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
